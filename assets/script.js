d2e623b1c8b0f24bcc894b9849c081d0f8717d17
joseph <josephcotterell@hotmail.co.uk>
2023-04-05 19:54:30 +0100

    added get function

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,32 @@
 console.log("Linked");
+const apiKey = "9cd06e16b3d3e32493947fc089bc851e";
 function searchApi(cityName) {
+  console.log("inside function", cityName);
+  let url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`;
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      let lat = data[0].lat;
+      let lon = data[0].lon;
+      getWeather(lat, lon);
+      console.log(lat, lon);
+    });
   console.log("searching api");
   console.log(cityName);
   //perform a fetch for that data with the city Name
+}
+function getWeather(lat, lon) {
+  let url = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
 }
 
 function handleFormSubmit(event) {
